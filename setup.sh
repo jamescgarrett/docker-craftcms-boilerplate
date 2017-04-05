a5748366af9a960c786d728f800f551096d1af02
James Garrett <jamescgarrett@gmail.com>
2017-04-05 14:39:10 -0400

    Moving docker-compose and nginx conf out of bash setup and adding .env config

--- setup.sh
+++ setup.sh
@@ -1,11 +1,15 @@
 {
 
+  backup_database() {
+    echo "Backing up database to ./docker/database"
+
+    echo "Finished Backing up database"
+  }
+
   craft_setup() {
     _craft_get_craft
     _create_configs
     _move_templates
-    _create_docker-compose
-    _create_nginx_conf
 
     echo "Running docker-compose up -d"
     docker-compose up -d
@@ -58,11 +62,12 @@
    */
 
   return array(
-    'server' => 'localhost',
-    'database' => 'projectname',
-    'user' => 'root',
-    'password' => 'secret',
-    'tablePrefix' => 'craft_',
+    'server' => getenv('DB_SERVER'),
+    'port' => getenv('DB_PORT'),
+    'database' => getenv('DB_NAME'),
+    'user' => getenv('DB_USER'),
+    'password' => getenv('DB_PASS'),
+    'tablePrefix' => getenv('DB_PREFIX'),
   );
 EOF
     echo "Finished Creating Database Config"
@@ -81,7 +86,7 @@ EOF
 
 return array(
   'enableCsrfProtection' => true,
-  'omitScriptNameInUrls' => 'auto',
+  'omitScriptNameInUrls' => true,
   'devMode' => true,
 );
 EOF
@@ -94,126 +99,10 @@ EOF
     rm -rf craft_templates
   }
 
-  _create_docker-compose() {
-    echo "Creating docker-compose.yml"
-    cat > ./docker-compose.yml <<- 'EOF'
-version: '2.1'
-
-services:
-  web:
-    image: nginx:latest
-    ports:
-      - "8080:80"
-    volumes:
-      - "${PUBLIC_ROOT}:/var/www/html"
-      - "${CRAFT_ROOT}:/var/www/craft"
-      - ./docker/nginx/default.conf:/etc/nginx/conf.d/default.conf
-    networks:
-      - public-network
-
-  php:
-    image: jamescgarrett/craftcms-php:latest
-    volumes:
-      - "${PUBLIC_ROOT}:/var/www/html"
-      - "${CRAFT_ROOT}:/var/www/craft"
-    networks:
-      - public-network
-
-  mysql:
-    image: mariadb:latest
-    volumes:
-      - data:/var/lib/mysql
-    environment:
-      MYSQL_ROOT_PASSWORD: "${MYSQL_ROOT_PASSWORD}"
-      MYSQL_DATABASE: "${MYSQL_DATABASE}"
-      MYSQL_USER: "${MYSQL_USER}"
-      MYSQL_PASSWORD: "${MYSQL_PASSWORD}"
-    networks:
-      - public-network
-
-volumes:
-  data:
-
-networks:
-  public-network:
-    driver: bridge
-EOF
-    echo "Finished creating docker-compose.yml"
-  }
-
-  _create_nginx_conf() {
-    echo "Creating nginx conf"
-    mkdir -p docker/nginx
-    cat > ./docker/nginx/default.conf <<- 'EOF'
-server {
-  listen 80 default_server;
-  listen [::]:80 default_server ipv6only=on;
-
-  index index.php index.html;
-  server_name localhost;
-  root /var/www/html;
-
-  # reduce the data that needs to be sent over network
-  gzip on;
-  gzip_min_length 10240;
-  gzip_proxied expired no-cache no-store private auth;
-  gzip_types text/plain text/css application/javascript application/json application/x-javascript text/xml application/xml application/xml+rss text/javascript;
-  gzip_disable "MSIE [1-6]\.";
-
-  # Add stdout logging
-  error_log /dev/stdout info;
-  access_log /dev/stdout;
-
-  location ~ [^/]\.php(/|$) {
-    fastcgi_split_path_info ^(.+?\.php)(/.*)$;
-    fastcgi_intercept_errors on;
-    fastcgi_pass php:9000;
-    fastcgi_index index.php;
-
-    include fastcgi_params;
-    fastcgi_param HTTP_PROXY "";
-    fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
-    fastcgi_param SCRIPT_NAME $fastcgi_script_name;
-    fastcgi_param PATH_INFO $fastcgi_path_info;
-    fastcgi_param PATH_TRANSLATED $document_root$fastcgi_path_info;
-  }
-
-  location ~ ^(.*)$ {
-    try_files $uri $uri/ /index.php?p=$uri&$args;
-  }
-
-  location ~* \.(?:jpg|jpeg|gif|png|ico|cur|gz|svg|svgz|mp4|ogg|ogv|webm|htc|ttf|ttc|otf|eot|woff)$ {
-    try_files $uri /index.php?$query_string;
-    expires max;
-    add_header Pragma public;
-    add_header Cache-Control "public, must-revalidate, proxy-revalidate";
-  }
-
-  location ~* \.(?:css|js)$ {
-    try_files $uri /index.php?$query_string;
-    expires 1y;
-    add_header Cache-Control "public";
-  }
-
-  location = /robots.txt  { access_log off; log_not_found off; }
-  location = /favicon.ico { access_log off; log_not_found off; }
-  location ~ /\. { access_log off; log_not_found off; deny all; }
-
-  location ~* (?:\.(?:bak|config|sql|fla|psd|ini|log|sh|inc|swp|dist)|~)$ {
-    deny all;
-    access_log off;
-    log_not_found off;
-  }
-}
-EOF
-    echo "Finished creating nginx conf"
-  }
-
   program_abort() {
     >&2 echo "[ERROR] $1"
     exit ${2:1}
   }
 
-  craft_setup
-
+  "$@"
 }
